02b64d2136bdad978a165333e7f916c7dbdad474
RyuJinHa <bb_jinha98@naver.com>
2018-11-22 15:28:16 +0900

    02실습

--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 int main(int argc, char *argv[]) {
 	
 	char src[100]="programming course";
-	char dst[100];
 	
-	int i;
+	
+	int i=0;
 	while(src[i]!='\0')
 	{
-		dst[i]=src[i];
 		i++;
 	}
-	dst[i]='\0';
-	printf("%s\n",dst);
+
+
+	printf("%s (%i)\n",src, i);
 	
 
 
